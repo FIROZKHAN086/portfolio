01f8d21aeca748daaa0a97c7809dc02e8ee62fcf
FIROZ KHAN <computerpc096@gmail.com>
2025-04-10 15:55:50 -0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import Navbar from './Components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Projects from './pages/Projects.jsx';
+import Contact from './pages/Contact.jsx';
 import { AnimatePresence } from 'framer-motion';
 
 function App() {
